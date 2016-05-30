dcdc1db70b56d20a37e77b92cfa0545385dda893
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2016-05-30 21:45:26 +0200

    fix apps list

--- apps/apply.zsh
+++ apps/apply.zsh
@@ -2,32 +2,34 @@ require brew-cask
 
 CASK_APPS=(
 	gpgtools
-    iterm2 
-    avast
-    vlc
-    google-chrome
-    google-drive
-    skype
-    adium 
-    sublime-text
-    osxfuse
-    evernote
-    java
-    qbittorrent
-    stellarium
-    gimp
-    inkscape
-    android-file-transfer
-    plex-media-server
-    plex-home-theater
+	iterm2 
+	avast
+	vlc
+	google-chrome
+	google-drive
+	skype
+	adium 
+	sublime-text
+	osxfuse
+	evernote
+	java
+	qbittorrent
+	stellarium
+	gimp
+	inkscape
+	android-file-transfer
+	plex-media-server
+	plex-home-theater
 )
 
 APPS=(
 	mc
+	jenv
  	pwgen
-    p7zip
+	p7zip
 	encfs
 	ntfs-3g
+	hexedit
 )
 
 for app in ${CASK_APPS[*]} 
