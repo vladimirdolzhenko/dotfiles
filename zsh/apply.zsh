dc1c3cac2e94740203f00220bbcd57f66cf8339f
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2016-06-01 21:25:06 +0200

    fix apply

--- zsh/apply.zsh
+++ zsh/apply.zsh
@@ -1,14 +1,23 @@
 require brew
 
-# git clone https://github.com/chriskempson/base16-shell.git
-# git clone https://github.com/tarjoilija/zgen.git
+DOT="${HOME}/dotfiles"
+
+cloneIfNeeded() {
+    if [ ! -d "${DOT}/zsh/${1}/.git" ]; then
+        git clone "${2}" "${DOT}/zsh/${1}"
+    fi
+}
+
+cloneIfNeeded zgen https://github.com/tarjoilija/zgen.git
+cloneIfNeeded base16-shell https://github.com/chriskempson/base16-shell.git
+cloneIfNeeded zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
 
 # clean
 rm -f ~/.zshrc
 rm -f ~/.zprofile
 
 # install
-brew install zsh
+brew list zsh || brew install zsh
 
 # configure
 [[ $SHELL == "/bin/zsh" ]] || chsh -s /bin/zsh
@@ -17,3 +26,5 @@ DIR=$(dirname $0:A)
 
 ln -s ${DIR}/zshrc ~/.zshrc
 ln -s ${DIR}/zprofile ~/.zprofile
+
+
