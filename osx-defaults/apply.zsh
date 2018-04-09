28f55ebac82ea181dbeb16dfdfadc8ce0a74b0a1
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2018-04-09 08:56:48 +0200

    update

--- osx-defaults/apply.zsh
+++ osx-defaults/apply.zsh
@@ -18,7 +18,7 @@ defaults write NSGlobalDomain InitialKeyRepeat -integer 15
 defaults write NSGlobalDomain KeyRepeat -integer 2
 
 # do not add period with double-space
-defaults write NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
 defaults write com.apple.HIToolbox AppleEnabledInputSources -array '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>0</integer><key>KeyboardLayout Name</key><string>U.S.</string></dict>'
 defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>-23011</integer><key>KeyboardLayout Name</key><string>Russian</string></dict>' 
