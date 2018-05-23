17ce2bad59f4c565f7c8db0568e4ce632f178958
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2018-05-23 20:23:36 +0200

    add iosevka font

--- fonts/apply.zsh
+++ fonts/apply.zsh
@@ -2,13 +2,14 @@ require brew
 
 brew tap caskroom/fonts
 FONTS=(
-	font-inconsolata 
-	font-input 
-	font-source-code-pro 
-	font-fira-code 
-	font-anonymous-pro 
-	font-awesome-terminal-fonts 
-	font-fantasque-sans-mono 
+	font-inconsolata
+	font-input
+	font-source-code-pro
+	font-fira-code
+	font-anonymous-pro
+	font-awesome-terminal-fonts
+	font-fantasque-sans-mono
+	font-iosevka
 	#font-sauce-code-powerline
 )
 
