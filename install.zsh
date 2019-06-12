6b1af138b4bcccfb138a301370cc1cdb5da5f76f
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2019-06-12 14:43:53 +0200

    JB edition2

--- install.zsh
+++ install.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-COMPUTER_NAME="Vladimir’s JetBrains MacBook Pro"
+COMPUTER_NAME="Vladimir’s MacBook Pro"
 HOST_NAME=$(echo $COMPUTER_NAME | sed "s, ,-,g;s,[^-a-zA-Z],,g")
 
 # https://github.com/myfreeweb/dotfiles
@@ -14,67 +14,67 @@ WARN="${fg[yellow]}==>${reset_color}"
 ERROR="${fg[red]}==>${reset_color}"
 
 is-macos(){
-    if [[ "$OSTYPE" =~ ^darwin ]]; then
-      return 0
-    fi
-    return 1
+	if [[ "$OSTYPE" =~ ^darwin ]]; then
+		return 0
+	fi
+	return 1
 }
 
 is-executable(){
-    if [ $commands[$*] ]; then
-        return 0
-    fi
-    return 1
+	if [ $commands[$*] ]; then
+		return 0
+	fi
+	return 1
 }
 
 require() {
-    if [ ! $commands[$*] ]; then
-        echo "$ERROR '$*' is not installed or not a command')"
-        exit 1
-    fi
+	if [ ! $commands[$*] ]; then
+		echo "$ERROR '$*' is not installed or not a command')"
+		exit 1
+	fi
 }
 
 # load modules
 
 MODULES=(
-    homebrew
-    git
-    nvim
-    zsh
-    bash
-    apps
-    dev
-    osx
-    fonts
-    tools
-    jetbrains
-    mc
-    sublime
+	homebrew
+	git
+	nvim
+	zsh
+	bash
+	apps
+	dev
+	#jetbrains
+	osx
+	fonts
+	tools
+	mc
+	sublime
 )
 
 if [[ -n "$@" ]]; then
-    MODULES="$@"
+	MODULES="$@"
 fi
 
 for m in $MODULES; do
-    module=$(echo $m | sed -E "s,(.*)(/.*),\1,g")
-    module_params=$(echo $m | cut -d'/' -s -f2)
-    if [[ -d "$module" ]]; then
-        if [[ -f "$module/apply.zsh" ]]; then
-            echo "$MSG Installing module '${fg_bold[white]}$module${reset_color}'"
-            (
-                cd "$module"
-                PWD=$(pwd)
-                if source apply.zsh "${module_params}" ; then
-                    echo "$INFO Module '$module' has been installed"
-                else
-                    echo "$ERROR Module '$module' failed"
-                fi
-            )
-        else
-            echo "$ERROR '$module' is not a module (missing apply.zsh)"
-        fi
-    else
-        echo "$ERROR '$module' is not a module directory"
-    fi
+	module=$(echo $m | sed -E "s,(.*)(/.*),\1,g")
+	module_params=$(echo $m | cut -d'/' -s -f2)
+	if [[ -d "$module" ]]; then
+		if [[ -f "$module/apply.zsh" ]]; then
+			echo "$MSG Installing module '${fg_bold[white]}$module${reset_color}'"
+			(
+				cd "$module"
+				PWD=$(pwd)
+				if source apply.zsh "${module_params}" ; then
+					echo "$INFO Module '$module' has been installed"
+				else
+					echo "$ERROR Module '$module' failed"
+				fi
+			)
+		else
+			echo "$ERROR '$module' is not a module (missing apply.zsh)"
+		fi
+	else
+		echo "$ERROR '$module' is not a module directory"
+	fi
 done
--- jetbrains/apply.zsh
+++ jetbrains/apply.zsh
@@ -1,15 +1,24 @@
 require brew
 
+export COMPUTER_NAME="Vladimir’s JetBrains MacBook Pro"
+
+DOTFILES_DIR=$(pwd -P)
+ln -sf "${DOTFILES_DIR}/zshrc.jetbrains" "${HOME}/.zshrc.custom"
+
 brew tap adoptopenjdk/openjdk
 
 CASK_APPS=(
 	slack
+	tunnelblick
 	java6
 	caskroom/versions/zulu7
 	#caskroom/versions/zulu8
 	adoptopenjdk9
 	adoptopenjdk10
 	adoptopenjdk11
+	oracle-jdk
+	eclipse-ide
+	docker
 )
 
 for app in ${CASK_APPS[*]}
@@ -19,5 +28,7 @@ done
 
 brew untap adoptopenjdk/openjdk
 
-# APPS=()
-# brew install ${APPS[*]}
+APPS=(
+	vault
+)
+brew install ${APPS[*]}
